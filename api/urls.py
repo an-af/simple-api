5c87e9834baf703bcc1c6c8aee8b7a92ba5798fb
an-af <ahnaf.fattah1@gmail.com>
2025-05-16 13:28:59 +0700

    Mengambi data berdasarkan ID

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("test", views.test, name="test"),
-    path("customer",views.getAllData, name="getAllData")
+    path("customer",views.getAllData, name="getAllData"),
+    path('customer/<int:customer_id>',views.getDataByID, name="getDataByID")
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,9 @@
 from django.http import JsonResponse
+from .models import Customer
+from django.forms.models import model_to_dict
 import datetime
 
+# testing
 def test(request):
     time = datetime.datetime.now()
     response = {
@@ -9,5 +12,21 @@ def test(request):
     }
     return JsonResponse(response)
 
+# get all Data
 def getAllData(request):
-    return JsonResponse({"msg":"getAllData"})
+    try:
+        data = Customer.objects.all().values('id','name','email','phone')
+        print(data)
+        return JsonResponse({"customers" : list(data)})
+    
+    except Exception as e:
+        return JsonResponse({"error":str(e)}, status=500)
+
+# get data By Id
+def getDataByID(request, customer_id):
+    try:
+        data = Customer.objects.get(id=customer_id)
+        data = model_to_dict(data)
+        return JsonResponse({"data":data}, status=200)
+    except Exception as e:
+        return JsonResponse({"error":str(e)}, status=500)
